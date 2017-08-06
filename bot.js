e9a5d438104d432e6787a69fd90ac7d78714d85f
GardenofSinners <GardenofSinners@users.noreply.github.com>
2017-08-06 17:31:34 +0100

    Updated bot for MySQL

--- bot.js
+++ bot.js
@@ -1,153 +1,99 @@
-const Eris = require("eris");
-var googl = require('goo.gl');
-var fs = require('fs');
-var request = require('request')
+// Authors: Elm & Tenshi
 
-googl.setKey(''); // Your Goo.gl API key.
-var bot = new Eris(""); // Discord bot account token.
-var IPBAPI = ""; // Your Invision Power Board API key. (GET request)
-var forumDomain = "www.example.com"; // Forum domain where Invision Power Board is located.
-var botPrefix = "!"; // Prefix that bot will be called with.
-var postMessage = "New post has been posted!"; // What message will come before linking to to new post.
-var threadMessage = "New thread has been posted!"; // What message will come before linking to to new thread.
-var forumActivityChannel = ""; // Channel where bot will post new threads and new posts.
-var announcamentsChannel = ""; // Channel where bot will post new announcemenets.
+const Eris    			= require("eris");
+const googl   			= require('goo.gl');
+const mysql   			= require('mysql');
+const request 			= require('request');
+let erisAPI  			= "replace with your discord bot token";
+let config 			= {};
+let latestThreadRequest 	= {};
+let latestPostRequest   	= {};
 
-var latestThreadRequest = {
-  url: 'https://'+forumDomain+'/api/index.php?/forums/topics&sortBy=date&sortDir=desc',
-  auth: {
-    user: IPBAPI,
-    password: ""
-  }
-}
+const bot = new Eris(erisAPI);
 
-var latestPostRequest = {
-  url: 'https://'+forumDomain+'/api/index.php?/forums/posts&sortBy=date&sortDir=desc',
-  auth: {
-    user: IPBAPI,
-    password: ""
-  }
-}
+//DO NOT EDIT BELOW THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING
 
-var latestThread = {
+let latestThread = {
     title: 'unknown',
     id: -1,
     author: 'unknown',
+    authorId: 'none',
     url: 'unknown',
-	content: 'none',
-	date: 'none'
+    content: 'none',
+    date: 'none'
 }
 
-var latestPost = {
+let latestPost = {
     id: -1,
     author: 'unknown',
+    authorId: 'none',
     url: 'unknown',
-	content: 'none',
-	date: 'none'
+    content: 'none',
+    date: 'none'
 }
 
+let connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'ipb'
+});
 
+connection.connect();
 
-var commands = {
-    "ping": {
-        usage: "",
-        description: "Returns time bot takes to respond.",
-        process: function (bot, msg, suffix, callback) {
-            var t = new Date();
-            bot.createMessage(msg.channel.id, "<@" + msg.author.id + "> pong (" + (t.getTime() - msg.timestamp) + "ms)")
-        }
-    },
-	"addNewTag": {
-		usage: "",
-		hidden: true,
-		process: function (bot, msg, suffix, callback) {
-            var splittedSuffix = suffix.split(" ");
-			if(msg.author.id != "282235475484475392" && msg.author.id != "136256776013086720")
-			{
-				return;
-			}
-			fs.readFile("userTags.json", "utf8", function(err, out) {
-				if (err) {
-					throw err;
-				}
-				var obj = JSON.parse(out);
-				if (!(splittedSuffix[0] in obj)) {
-					obj[splittedSuffix[0]] = {};
-					obj[splittedSuffix[0]].userTag = splittedSuffix[1];
-					console.log(obj[splittedSuffix[0]]);
-					fs.writeFile("userTags.json", JSON.stringify(obj, null, 4), function(err) {
-                    if (err) {
-                        throw err;
-					}
-					bot.createMessage(msg.channel.id, "New user added");
-                    });
-				}
-			});
-        }                               
+connection.query('SELECT * FROM config', function (error, results, fields) {
+	if (error) throw error;
+	config = {
+		googlAPI: results[0].googlAPI,
+		ipbAPI: results[0].ipbAPI,
+		domain: results[0].domain,
+		botPrefix: results[0].botPrefix,
+		postMessage: results[0].postMessage,
+		threadMessage: results[0].threadMessage,
+		activityChannel: results[0].activityChannel,
+		admins: results[0].admins.split(","),
+		welcome: results[0].welcomeChannel,
+		autoRoleEnabled: results[0].autoRoleEnabled,
+		roleID: results[0].roleId
 	}
-};
+});
 
+bot.connect();
 
 bot.on("ready", () => {
-
-    GetLatestThread();
+	console.log("Bot connected");
+	googl.setKey(config.googlAPI);
+	latestThreadRequest = {
+		url: `https://${config.domain}/api/index.php?/forums/topics&sortBy=date&sortDir=desc`,
+		auth: {
+			user: config.ipbAPI,
+			password: ""
+		}
+	};
+	latestPostRequest = {
+		url: `https://${config.domain}/api/index.php?/forums/posts&sortBy=date&sortDir=desc`,
+		auth: {
+			user: config.ipbAPI,
+			password: ""
+		}
+	};
+	GetLatestThread();
     GetLatestPost();
 
     setInterval(GetLatestPost, 9000);  //Sends a request every 9 seconds
     setInterval(GetLatestPost, 10000); //Sends a request every ten seconds
+    setInterval(GetLatestThread, 9000);  //Sends a request every 9 seconds
+    setInterval(GetLatestThread, 10000); //Sends a request every ten seconds
 });
 
-bot.on("messageCreate", (msg) => {
-    if (msg.channel.type == 1) return; // Bot will not react to messages sent in Direct Messages to avoid errors.
+bot.on("guildMemberAdd", (guild, member) => { // When a member joins a guild
 
-    var guildId = msg.channel.guild.id;
-
-    if (msg.author.id != bot.user.id && msg.content.slice(0, botPrefix.length) == botPrefix) {
-        var cmdTxt = msg.content.split(" ")[0].substring(botPrefix.length);
-        var suffix = msg.content;
-        var asuffix = suffix.split(" ");
-        var suffix = asuffix;
-        
-        suffix.shift();
-        suffix.join(":");
-        suffix = "" + suffix;
-        suffix = suffix.replace(",", " ");
-
-        //Help command
-        if (cmdTxt === "help") {
-            var i;
-            var helpArray = "";
-            
-            for (var cmd in commands) {
-                var info = botPrefix + cmd;
-                var usage = commands[cmd].usage;
-                var description = commands[cmd].description;
-                var hidden = commands[cmd].hidden;
-                var command;
-
-                if (hidden) continue;
-
-                if (usage !== "") { command = info + " (Usage: " + usage + ") - " + description + "\n"; } else {
-                    command = info + " - " + description + "\n";
-                }
-
-                helpArray += command;
-            }
-
-            bot.getDMChannel(msg.author.id).then(privateChannel => {
-            bot.createMessage(privateChannel.id, "" + helpArray);
-            });
-
-        } else {
-            var cmd = commands[cmdTxt];
-            if (cmd === undefined) return;
-            cmd.process(bot, msg, suffix);
-
-        }
-
-
-    }
-
+	if(!config.autoRoleEnabled) return;
+    let response = `Welcome ${member.mention} to ${guild.name}! Please visit our forum if you haven't already! (${config.domain})`;
+    let reason = 'Automated by Bot.';
+	guild.addMemberRole(member.id, config.roleID, reason);
+    bot.createMessage(config.welcome, response);
+    
 });
 
 function GetLatestThread()
@@ -159,9 +105,9 @@ function GetLatestThread()
             return
         }
 
-        var jsonBody = JSON.parse(body);
-		latestThread.date = jsonBody["results"][0]['firstPost']['date'];
-		latestThread.content = jsonBody['results'][0]['firstPost']['content'];
+        let jsonBody = JSON.parse(body);
+        latestThread.date = jsonBody["results"][0]['firstPost']['date'];
+        latestThread.content = jsonBody['results'][0]['firstPost']['content'];
 
         if (jsonBody["results"][0]['id'] != latestThread.id && latestThread.id != -1)
         {
@@ -169,13 +115,8 @@ function GetLatestThread()
             latestThread.id = jsonBody["results"][0]['id'];
             latestThread.author = jsonBody["results"][0]['firstPost']['author']['name'];
             latestThread.url = jsonBody["results"][0]['url'];
-
-            
-            if (jsonBody["results"][0]['forum']['name'] == "Announcements"){
-                AnnounceAnnouncement();
-            } else {
-                AnnounceLatestThread();
-            }
+            latestThread.authorId = jsonBody["results"][0]["firstPost"]["author"]["customFields"]["1"]["fields"]["2"]["value"];
+            AnnounceLatestThread();
 
         }
         if (jsonBody["results"][0]['id'] != latestThread.id && latestThread.id == -1){
@@ -183,6 +124,7 @@ function GetLatestThread()
             latestThread.id = jsonBody["results"][0]['id'];
             latestThread.author = jsonBody["results"][0]['firstPost']['author']['name'];
             latestThread.url = jsonBody["results"][0]['url'];
+            latestThread.authorId = jsonBody["results"][0]["firstPost"]["author"]["customFields"]["1"]["fields"]["2"]["value"];
         }
     })
 }
@@ -195,12 +137,13 @@ function GetLatestPost()
             return
         }
 
-        var jsonBody = JSON.parse(body);
+        let jsonBody = JSON.parse(body);
 
         if (jsonBody["results"][0]['id'] != latestPost.id && latestPost.id != -1 && latestThread.date != jsonBody["results"][0]['date'] && latestThread.content != jsonBody["results"][0]['content']){
             latestPost.id = jsonBody["results"][0]['id'];
             latestPost.author = jsonBody["results"][0]['author']['name'];
             latestPost.url = jsonBody["results"][0]['url'];
+            latestPost.authorId = jsonBody["results"][0]["author"]["customFields"]["1"]["fields"]["2"]["value"];
             AnnounceLatestPost();
         }
 
@@ -208,76 +151,52 @@ function GetLatestPost()
             latestPost.id = jsonBody["results"][0]['id'];
             latestPost.author = jsonBody["results"][0]['author']['name'];
             latestPost.url = jsonBody["results"][0]['url'];
+            latestPost.authorId = jsonBody["results"][0]["author"]["customFields"]["1"]["fields"]["2"]["value"];
         }
     })
+
 }
 
 function AnnounceLatestThread(link){
-GetShortLink(latestThread.url, 1)
-}
-
-function AnnounceAnnouncement(link){
-	GetShortLink(latestThread.url, 2)
+    GetShortLink(latestThread.url, 1)
 }
 
 function AnnounceLatestPost(link){
-	GetShortLink(latestPost.url, 3)
+    GetShortLink(latestPost.url, 2)
 }
-
 
 function GetShortLink(urlToShorten, whatIsShortening) {
-	googl.shorten(urlToShorten)
-		.then(function (shortUrl) {
-			switch(whatIsShortening) {
-				case 1: 
-					fs.readFile("userTags.json", "utf8", function(err, out) {
-						 if (err) {
-							throw err;
-						}
-						var obj = JSON.parse(out);
-						if ((latestThread.author in obj)) {
-							bot.createMessage(forumActivityChannel, threadMessage+"\n" + shortUrl + " by " + obj[latestThread.author].userTag );
+    googl.shorten(urlToShorten)
+        .then(function (shortUrl) {
+			if(whatIsShortening == 1)
+			{
+				connection.query(`SELECT * FROM usertags WHERE userName='${latestThread.author}'`, function (error, results, fields) {
+					if (error) throw error;
+					if(results.length != 0) {
+						bot.createMessage(config.activityChannel, `${config.threadMessage}\n${shortUrl} by <@${results[0].discordId}>`);
+					} else {
+						if(!latestThread.authorId) {
+							bot.createMessage(config.activityChannel, `${config.threadMessage}\n${shortUrl} by ${latestThread.author}`);
 						} else {
-							bot.createMessage(forumActivityChannel, threadMessage+"\n" + shortUrl + " by " + latestThread.author );
+							bot.createMessage(config.activityChannel, `${config.threadMessage}\n${shortUrl} by <@${latestThread.authorId}>`);						
 						}
-					});
-					break;
-				case 2:
-					fs.readFile("userTags.json", "utf8", function(err, out) {
-						 if (err) {
-							throw err;
-						}
-						var obj = JSON.parse(out);
-						if ((latestThread.author in obj)) {
-							bot.createMessage(forumActivityChannel, threadMessage+"\n " + shortUrl + " by " +obj[latestThread.author].userTag);
+					}
+				});	
+			} 
+			else if(whatIsShortening == 2) 
+			{ 
+				connection.query(`SELECT * FROM usertags WHERE userName='${latestPost.author}'`, function (error, results, fields) {
+					if (error) throw error;
+					if(results.length != 0) {
+						bot.createMessage(config.activityChannel, `${config.postMessage}\n${shortUrl} by <@${results[0].discordId}>`);
+					} else {
+						if(!latestPost.authorId) {
+							bot.createMessage(config.activityChannel, `${config.postMessage}\n${shortUrl} by ${latestPost.author}`);
 						} else {
-							bot.createMessage(forumActivityChannel, threadMessage+" \n " + shortUrl + " by " +latestThread.author);
+							bot.createMessage(config.activityChannel, `${config.postMessage}\n${shortUrl} by <@${latestPost.authorId}>`);
 						}
-					});
-					break;
-				case 3:
-					fs.readFile("userTags.json", "utf8", function(err, out) {
-						 if (err) {
-							throw err;
-						}
-						var obj = JSON.parse(out);
-						if ((latestPost.author in obj)) {
-							bot.createMessage(forumActivityChannel, postMessage+"\n" + shortUrl + " by " + obj[latestPost.author].userTag);
-						} else {
-							bot.createMessage(forumActivityChannel, postMessage+"\n" + shortUrl + " by " + latestPost.author);
-						}
-					});
-					break;
-				default: 
-					console.log("GetShortLink() wasn't called properly.");
-					break;
+					}
+				});				
 			}
-		})
-		.catch(function (err) {
-			console.error(err.message);
 	});
 }
-
-
-
-bot.connect(); // Get the bot to connect to Discord
